bff698b28bbadac55c3983b00b60a592bf115b0b
Illia <ilyas.sporichev@gmail.com>
2020-01-14 21:27:22 +0300

    base functional added

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/buttons', (req, res) => res.render('pages/buttons-list', { buttons: [
+          { id: `misha-ratue-${Math.random().toString(36).substr(2, 9)}`, audioSrc: `/buttons-content/misha-ratue.ogg`, avatar: '/buttons-content/misha-ratue.jpg' },
+          { id: `andrei-karasique-${Math.random().toString(36).substr(2, 9)}`, audioSrc: '/buttons-content/andrei-karasique.ogg', avatar: '/buttons-content/andrei-karasique.jpg' },
+      ] }))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
