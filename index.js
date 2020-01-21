45c4e7d600314280d90c39ebee5322acadc7b76f
Illia <ilyas.sporichev@gmail.com>
2020-01-21 15:59:42 +0300

    background added

--- index.js
+++ index.js
@@ -2,27 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-const http = require('https');
 
-const { getAwsDataList } = require('./services/get-aws-data-list');
 const { generateRenderInstance } = require('./services/gen-render-instance');
-
 const { setupMongoose } = require('./init/setup-mongo');
-const { SavedUser } = require('./models');
-
-const httpGet = url => {
-    return new Promise((resolve, reject) => {
-        http.get(url, res => {
-            const chunksArray = [];
-            let length = 0;
-            res.on('data', chunk => {
-                chunksArray.push(chunk);
-                length += chunk.length;
-            });
-            res.on('end', () => resolve(Buffer.concat(chunksArray), length));
-        }).on('error', reject);
-    });
-};
 
 (async function () {
     await setupMongoose();
@@ -33,20 +15,10 @@ const httpGet = url => {
         .set('view engine', 'ejs')
         .get('/', (req, res) => res.render('pages/index'))
         .get('/buttons', async (req, res) => {
-            const usersArr = await SavedUser.find({});
 
-            const [audioArr, avatarArr] = await Promise.all([
-                Promise.all(usersArr.map(({ audioUrl}) => httpGet(`https://api.telegram.org/file/bot${process.env.TG_TOKEN}/${audioUrl}`))),
-                Promise.all(usersArr.map(({ avatarUrl }) => httpGet(`https://api.telegram.org/file/bot${process.env.TG_TOKEN}/${avatarUrl}`))),
-            ]);
-
-            const renderInstance = audioArr.map((audioBuffer, index) => ({
-                id: `_${Math.random().toString(36).substr(2, 9)}`,
-                audioBuffer,
-                avatarBuffer: avatarArr[index],
-            }));
+            const renderInstance = await generateRenderInstance()
             res.render('pages/buttons-list', { buttons: JSON.stringify(renderInstance)})
         })
-        .listen(PORT, "0.0.0.0", () => console.log(`Listening on ${ PORT }`));
+        .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 })();
--- services/gen-render-instance.js
+++ services/gen-render-instance.js
@@ -1,9 +1,36 @@
-const generateRenderInstance = function ({ audioCollection, avatarCollection }) {
-    return audioCollection.map((audioBuffer, index) => ({
+const http = require('https');
+
+const { SavedUser } = require('../models');
+
+const httpGet = url => {
+    return new Promise((resolve, reject) => {
+        http.get(url, res => {
+            const chunksArray = [];
+            let length = 0;
+            res.on('data', chunk => {
+                chunksArray.push(chunk);
+                length += chunk.length;
+            });
+            res.on('end', () => resolve(Buffer.concat(chunksArray), length));
+        }).on('error', reject);
+    });
+};
+
+const generateRenderInstance = async function () {
+    const usersArr = await SavedUser.find({});
+
+    const [audioArr, avatarArr] = await Promise.all([
+        Promise.all(usersArr.map(({ audioUrl}) => httpGet(`https://api.telegram.org/file/bot${process.env.TG_TOKEN}/${audioUrl}`))),
+        Promise.all(usersArr.map(({ avatarUrl }) => httpGet(`https://api.telegram.org/file/bot${process.env.TG_TOKEN}/${avatarUrl}`))),
+    ]);
+
+    const renderInstance = audioArr.map((audioBuffer, index) => ({
         id: `_${Math.random().toString(36).substr(2, 9)}`,
         audioBuffer,
-        avatarBuffer: avatarCollection[index],
-    }))
+        avatarBuffer: avatarArr[index],
+    }));
+
+    return renderInstance;
 };
 
 module.exports = { generateRenderInstance };
